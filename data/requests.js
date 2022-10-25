440ec4faecf8002b33006413977779495c2e7853
wilson <jwmrf@cin.ufpe.br>
2022-10-25 01:19:03 -0300

    QuestionByTag

--- data/requests.js
+++ data/requests.js
@@ -8,6 +8,14 @@ module.exports = class Requests {
         return api.get(`questions?order=desc&sort=${sort}&site=stackoverflow`)
     }
 
+    getLastQuestionByTag (tag = '') {
+        if (!tag) {
+            return api.get(`questions?pagesize=1&order=desc&sort=creation&site=stackoverflow`)
+        } else {
+            return api.get(`questions?pagesize=1&order=desc&sort=creation&tagged=${tag}&site=stackoverflow`)
+        }
+    }
+
     getTags (sort = 'popular') {
         return api.get(`tags?order=desc&sort=${sort}&site=stackoverflow`)
     }
--- data/routes.js
+++ data/routes.js
@@ -7,16 +7,23 @@ client.connect(4000, 'localhost', () => {
 
 })
 router.get('/questions',  async function (req, res) {
-    var questions = await requests.getQuestions()
+    let sort = req.query.sort ? req.query.sort : 'activity'
+    var questions = await requests.getQuestions(sort)
     client.write(JSON.stringify(questions.data.items))
     client.on('data', data => {
         res.end(data)
     })
-
 })
 
 router.get('/tags',  async function (req, res) {
-    var questions = await requests.getQuestions()
+    let sort = req.query.sort ? req.query.sort : 'popular'
+    var questions = await requests.getTags(sort)
+    res.send(JSON.stringify(questions.data.items))
+})
+
+router.get('/questionByTag',  async function (req, res) {
+    let tag = req.query.tag ? req.query.tag : ''
+    var questions = await requests.getLastQuestionByTag(tag)
     res.send(JSON.stringify(questions.data.items))
 })
 
